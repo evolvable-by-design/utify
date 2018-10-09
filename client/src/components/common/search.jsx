a6904515053bbe1d1ce99ddff5c4d2254ad031ef
georgejacobt <36488006+georgejacobt@users.noreply.github.com>
2018-10-09 11:19:16 -0500

    search list grid working

--- client/src/components/common/search.jsx
+++ client/src/components/common/search.jsx
@@ -4,6 +4,35 @@ import Grid from "@material-ui/core/Grid";
 import API from "../../utils/API";
 import SearchTopNav from "./searchTopNav";
 
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from "@material-ui/core/IconButton";
+import Favorite from "@material-ui/icons/Favorite";
+import PlayArrow from "@material-ui/icons/PlayArrow";
+import PlaylistAdd from "@material-ui/icons/PlaylistAdd";
+import Tooltip from "@material-ui/core/Tooltip";
+
+const styles = theme => ({
+  gridRoot: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    width: "60%",
+    height: "auto"
+  },
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)"
+  }
+});
+
 class Search extends Component {
   state = {
     searchKeyword: this.props.location.state.referrer.searchKeyword,
@@ -36,30 +65,49 @@ class Search extends Component {
 
   render() {
     console.log(this.state.searchKeyword);
+    const { classes } = this.props;
     return (
       <React.Fragment>
         <Grid container direction="row" alignItems="center" justify="center">
           <SearchTopNav />
         </Grid>
-
-        {this.state.redirect ? (
-          <Grid container direction="row" alignItems="center" justify="center">
-            Search Results here
-            {this.state.searchResults.map(searchResult => (
-              <li key={searchResult.id.videoId}>
-                {searchResult.snippet.title}
-              </li>
-            ))}
-          </Grid>
-        ) : (
-          <div>
-            Your search results from seach bar on search page is here . Not a
-            redirect
+        <Grid container direction="row" alignItems="center" justify="center">
+          <div className={classes.gridRoot}>
+            <GridList cellHeight={180} className={classes.gridList} cols={4}>
+              <GridListTile key="Subheader" cols={4} style={{ height: "auto" }}>
+                <ListSubheader component="div">
+                  Showing Results for {this.state.searchKeyword}
+                </ListSubheader>
+              </GridListTile>
+              {this.state.searchResults.map(searchResult => (
+                <GridListTile key={searchResult.id.videoId}>
+                  <img
+                    src={searchResult.snippet.thumbnails.medium.url}
+                    alt={searchResult.snippet.channelTitle}
+                  />
+                  <GridListTileBar
+                    title={searchResult.snippet.title}
+                    actionIcon={
+                      <Tooltip title="Add to Watch List" placement="top-start">
+                        <IconButton className={classes.icon}>
+                          <PlaylistAdd />
+                        </IconButton>
+                      </Tooltip>
+                    }
+                  />
+                </GridListTile>
+              ))}
+            </GridList>
           </div>
-        )}
+        </Grid>
+        {/* Ternary goes here */}
       </React.Fragment>
     );
   }
 }
 
-export default Search;
+Search.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Search);
