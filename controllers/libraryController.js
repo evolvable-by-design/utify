130d30a293f8703bf3d07fa1eca051a10c9d8cb0
georgejacobt <36488006+georgejacobt@users.noreply.github.com>
2018-10-10 11:11:23 -0500

    watchlistsave to db works

--- controllers/libraryController.js
+++ controllers/libraryController.js
@@ -1,16 +1,20 @@
-require("../libraryMongoose")();
-const User = require("mongoose").model("Library");
+const db = require("../models");
 
 module.exports = {
   saveToLibrary: function(req, res) {
     let userid = req.body.userid;
     let videoRecord = req.body.videoRecord;
     console.log("this is " + userid + " and their Video Record" + videoRecord);
-    // User.findOne({ _id: userid })
-
-    //   .then(dbModel => {
-    //     console.log(dbModel);
-    //     res.json(dbModel);
-    //   });
+    console.log("test vedioREcord " + videoRecord.id.videoId);
+    db.LibraryItem.create({
+      userid: userid,
+      videoId: videoRecord.id.videoId,
+      thumbnailUrl: videoRecord.snippet.thumbnails.medium.url,
+      channelTitle: videoRecord.snippet.channelTitle,
+      title: videoRecord.snippet.title
+    }).then(dbModel => {
+      console.log(dbModel);
+      res.json(dbModel);
+    });
   }
 };
