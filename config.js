05aa8af4da740626c3008e5fe2215e1844fc94f3
georgejacobt <36488006+georgejacobt@users.noreply.github.com>
2018-10-09 23:28:46 -0500

    deploy test

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleAuth: {
+    clientID:
+      "524617566731-urp59hdl1t7q6pkrm5r45nsakul7e8nu.apps.googleusercontent.com",
+    clientSecret: "QBLJP63YwkZxPuTa4U1tR7h4",
+    callbackURL: "http://localhost:3001/auth/google/callback"
+  },
+  session: {
+    cookieKey: "utifyisgoingtobegreat"
+  }
+};
