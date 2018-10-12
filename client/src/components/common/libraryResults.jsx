1be82629d11ee22213b133a91361ee51cbcdd56d
georgejacobt <36488006+georgejacobt@users.noreply.github.com>
2018-10-12 09:56:29 -0500

    more styles work

--- client/src/components/common/libraryResults.jsx
+++ client/src/components/common/libraryResults.jsx
@@ -15,6 +15,7 @@ import Favorite from "@material-ui/icons/Favorite";
 import PlayArrow from "@material-ui/icons/PlayArrow";
 import PlaylistAdd from "@material-ui/icons/PlaylistAdd";
 import Tooltip from "@material-ui/core/Tooltip";
+import Typography from "@material-ui/core/Typography";
 
 import Button from "@material-ui/core/Button";
 import Snackbar from "@material-ui/core/Snackbar";
@@ -30,12 +31,12 @@ const styles = theme => ({
     backgroundColor: theme.palette.background.paper
   },
   root: {
-    width: "60%",
-    marginBottom: 30
+    width: "60%"
   },
   gridList: {
     width: "90%",
-    height: "auto"
+    height: "auto",
+    paddingBottom: 30
   },
   icon: {
     color: "rgba(255, 255, 255, 0.54)"
@@ -45,6 +46,17 @@ const styles = theme => ({
   },
   subHeader: {
     marginLeft: -22
+  },
+  subheadingStyle: {
+    marginLeft: 55,
+    fontWeight: "bold",
+    paddingTop: 10,
+    paddingBottom: 5
+  },
+  uhohStyle: {
+    width: "60%",
+    height: 300,
+    backgroundColor: "white"
   }
 });
 
@@ -77,6 +89,8 @@ class LibraryResults extends Component {
 
   render() {
     console.log(this.state.libraryResults);
+    const resultsLength = this.state.libraryResults.length;
+    console.log(resultsLength);
     const { classes } = this.props;
     const urlPrefix = "https://www.youtube.com/watch?v=";
     return (
@@ -88,26 +102,46 @@ class LibraryResults extends Component {
           justify="center"
           className={classes.root}
         >
-          <div className={classes.gridRoot}>
-            <GridList cellHeight={180} className={classes.gridList} cols={4}>
-              <GridListTile key="Subheader" cols={4} style={{ height: "auto" }}>
-                <ListSubheader component="div" className={classes.subHeader}>
-                  Watch List:
-                </ListSubheader>
-              </GridListTile>
-              {this.state.libraryResults.map(libraryResult => (
-                <GridListTile key={libraryResult.videoId}>
-                  <a target="_blank" href={urlPrefix + libraryResult.videoId}>
-                    <img
-                      src={libraryResult.thumbnailUrl}
-                      alt={libraryResult.channelTitle}
-                    />
-                  </a>
-                  <GridListTileBar title={libraryResult.title} />
+          {resultsLength ? (
+            <div className={classes.gridRoot}>
+              <Grid
+                container
+                direction="row"
+                className={classes.subheadingStyle}
+              >
+                Your Watch List
+              </Grid>
+              <GridList cellHeight={180} className={classes.gridList} cols={4}>
+                <GridListTile
+                  key="Subheader"
+                  cols={4}
+                  style={{ height: "auto" }}
+                >
+                  {/* <ListSubheader component="div" className={classes.subHeader}>
+                    Your Watch List:
+                  </ListSubheader> */}
                 </GridListTile>
-              ))}
-            </GridList>
-          </div>
+                {this.state.libraryResults.map(libraryResult => (
+                  <GridListTile key={libraryResult.videoId}>
+                    <a target="_blank" href={urlPrefix + libraryResult.videoId}>
+                      <img
+                        src={libraryResult.thumbnailUrl}
+                        alt={libraryResult.channelTitle}
+                      />
+                    </a>
+                    <GridListTileBar title={libraryResult.title} />
+                  </GridListTile>
+                ))}
+              </GridList>
+            </div>
+          ) : (
+            <div classeName={classes.uhohStyle}>
+              <Typography component="h2" variant="h1" gutterBottom>
+                Uh Oh! You dont have any videos saved! Try searching for
+                something now!
+              </Typography>
+            </div>
+          )}
         </Grid>
         {/* Ternary goes here */}
       </React.Fragment>
--- client/src/components/common/search.jsx
+++ client/src/components/common/search.jsx
@@ -39,6 +39,12 @@ const styles = theme => ({
   },
   close: {
     padding: theme.spacing.unit / 2
+  },
+  subheadingStyle: {
+    marginLeft: 55,
+    fontWeight: "bold",
+    paddingTop: 10,
+    paddingBottom: 5
   }
 });
 
@@ -113,11 +119,14 @@ class Search extends Component {
         </Grid>
         <Grid container direction="row" alignItems="center" justify="center">
           <div className={classes.gridRoot}>
+            <Grid container direction="row" className={classes.subheadingStyle}>
+              Showing Results for "{this.state.searchKeyword}"
+            </Grid>
             <GridList cellHeight={180} className={classes.gridList} cols={4}>
               <GridListTile key="Subheader" cols={4} style={{ height: "auto" }}>
-                <ListSubheader component="div">
+                {/* <ListSubheader component="div">
                   Showing Results for {this.state.searchKeyword}
-                </ListSubheader>
+                </ListSubheader> */}
               </GridListTile>
               {this.state.searchResults.map(searchResult => (
                 <GridListTile key={searchResult.id.videoId}>
