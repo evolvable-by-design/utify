8a08f562f861d07d9a4697593a381ae4fda3cea9
Antoine Cheron <cheron.antoine@gmail.com>
2020-09-14 14:44:31 +0200

    Replay API evolution from commit 4f64b31

--- mongoose.js
+++ mongoose.js
@@ -3,65 +3,64 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-module.exports = function() {
-  var UserSchema = new Schema({
-    fullName: {
-      type: String
+var UserSchema = new Schema({
+  fullName: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  },
+  googleProvider: {
+    type: {
+      id: String,
+      token: String
     },
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-      unique: true,
-      match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    select: false
+  }
+});
+
+UserSchema.set("toJSON", { getters: true, virtuals: true });
+
+UserSchema.statics.upsertGoogleUser = function(
+  accessToken,
+  refreshToken,
+  profile,
+  cb
+) {
+  console.log(profile);
+  var that = this;
+  return this.findOne(
+    {
+      "googleProvider.id": profile.id
     },
-    googleProvider: {
-      type: {
-        id: String,
-        token: String
-      },
-      select: false
-    }
-  });
+    function(err, user) {
+      // no user was found, lets create a new one
+      if (!user) {
+        var newUser = new that({
+          fullName: profile.displayName,
+          email: profile.emails[0].value,
+          googleProvider: {
+            id: profile.id,
+            token: accessToken
+          }
+        });
 
-  // UserSchema.set("toJSON", { getters: true, virtuals: true });
-
-  UserSchema.statics.upsertGoogleUser = function(
-    accessToken,
-    refreshToken,
-    profile,
-    cb
-  ) {
-    console.log(profile);
-    var that = this;
-    return this.findOne(
-      {
-        "googleProvider.id": profile.id
-      },
-      function(err, user) {
-        // no user was found, lets create a new one
-        if (!user) {
-          var newUser = new that({
-            fullName: profile.displayName,
-            email: profile.emails[0].value,
-            googleProvider: {
-              id: profile.id,
-              token: accessToken
-            }
-          });
-
-          newUser.save(function(error, savedUser) {
-            if (error) {
-              console.log(error);
-            }
-            return cb(error, savedUser);
-          });
-        } else {
-          return cb(err, user);
-        }
+        newUser.save(function(error, savedUser) {
+          if (error) {
+            console.log(error);
+          }
+          return cb(error, savedUser);
+        });
+      } else {
+        return cb(err, user);
       }
-    );
-  };
-
-  mongoose.model("User", UserSchema);
+    }
+  );
 };
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,9 +2,19 @@ const router = require("express").Router();
 const searchController = require("../controllers/searchController");
 const path = require('path')
 const YAML = require('yamljs')
+const { withAuth } = require("../utils/authentication.utils")
 
-const apiDocumentation = YAML.load(path.join(__dirname, '../openapi-v1.yaml'))
-router.route("/documentation").get((_, res) => res.status(200).json(apiDocumentation))
+const apiDocumentationV2WithoutDefaultValues = YAML.load(path.join(__dirname, '../openapi-v2.yaml'))
+
+router.route("/documentation").get(withAuth((req, res) => {
+  const userid = req.user.id
+  const userAdaptedDocumentation = JSON.parse(
+    JSON.stringify(apiDocumentationV2WithoutDefaultValues)
+      .replace(new RegExp('\\$context.userid', 'g'), userid)
+    )
+
+  res.status(200).json(userAdaptedDocumentation)
+}))
 
 //matches with "/api/search"
 
